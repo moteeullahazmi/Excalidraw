df86e3b77c89cdc53cc15f51906791af4879f341
moteeullahazmi <moteeullahazmi@gmail.com>
2025-01-13 17:24:11 +0530

    add websocket configuration

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,1 +1,32 @@
-console.log("http- server")
+import  express  from "express";
+import jwt from "jsonwebtoken"
+import {middleware} from "./middleware"
+import { JWT_SECRET } from "./config";
+
+const app = express()
+
+
+app.post("/signup", (req,res)=>{
+// db call
+res.json({
+    userId: "123"
+})
+})
+
+app.post("/signin", (req,res)=>{
+    const userId = 1;
+    const token = jwt.sign ({
+        userId
+    }, JWT_SECRET)
+
+    res.json({
+        token
+    })
+})
+
+app.post("/room",middleware, (req,res)=>{
+    // db call
+    res.json({
+        roomId:123
+    })
+})
